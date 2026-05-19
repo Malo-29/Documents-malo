d52776e340c28fdedca6ea2cc8f6a127fd1d283a
Malo-29 <malo-perso@riseup.net>
2026-05-19 23:47:26 +0200

    title only

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/Malo-29/Documents-malo",
-      Contact: "mailto:malo-perso@riseup.net",
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
