28f427cf8314911967420f275a81a0fb0e7d641a
Malo-29 <malo-perso@riseup.net>
2026-05-19 23:07:39 +0200

    update 2026-05-19

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,7 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
-// components shared across all pages
 export const sharedLayout: SharedLayout = {
   head: Component.Head(),
   header: [],
@@ -14,7 +13,18 @@ export const sharedLayout: SharedLayout = {
   }),
 }
 
-// components for pages that display a single page (e.g. a note)
+export const sharedPageComponents = {
+  head: Component.Head(),
+  header: [],
+  afterBody: [],
+  footer: Component.Footer({
+    links: {
+      GitHub: "https://github.com/Malo-29/Documents-malo",
+      Contact: "mailto:malo-perso@riseup.net",
+    },
+  }),
+}
+
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
@@ -36,7 +46,6 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 }
 
-// components for pages that display lists of pages (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
@@ -51,15 +60,4 @@ export const defaultListPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
-}
-
-export const sharedPageComponents = {
-  head: Component.Head(),
-  header: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/Malo-29/Documents-malo",
-      Contact: "mailto:malo-perso@riseup.net",
-    },
-  }),
 }
