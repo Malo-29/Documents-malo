a1d2ca30118618165d70030bf0313bbe1e9e95ac
Malo-29 <malo-perso@riseup.net>
2026-05-19 23:13:31 +0200

    fix layout

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,19 +1,7 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
-export const sharedLayout: SharedLayout = {
-  head: Component.Head(),
-  header: [],
-  afterBody: [],
-  footer: Component.Footer({
-    links: {
-      GitHub: "https://github.com/Malo-29/Documents-malo",
-      Contact: "mailto:malo-perso@riseup.net",
-    },
-  }),
-}
-
-export const sharedPageComponents = {
+export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
@@ -27,7 +15,10 @@ export const sharedPageComponents = {
 
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.ConditionalRender({
+      component: Component.Breadcrumbs(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
